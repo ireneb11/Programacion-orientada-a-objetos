db666e00ab83a2721d09e879198e9748ade11238
ireneb11 <irene.borja.u@gmail.com>
2025-09-29 12:12:54 +0200

    Update Ejercicio9.cs

--- Tema 1/Ejercicio9.cs
+++ Tema 1/Ejercicio9.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Personaje{
 // atributos de todos los personajes
-  private string nombre;
+  private string nombre {get;set};
   private int nivel, puntosDeVida;
 
 // constructor
@@ -11,6 +11,10 @@ public Personaje (string nombre, int nivel, int puntosDeVida){
   this.nivel=nivel;
   this.puntosDeVida=puntosDeVida;
 }
+
+public virtual mostrarInfo(){
+  getNombre();
+  
   
 }
 
@@ -20,5 +24,30 @@ public class Guerrero: Personaje{
   // Constructor
   public Guerrero (string nombre, int nivel, int puntosDeVida, float fuerza): base (nombre, nivel, puntosDeVida){
     this.fuerza= fuerza;
+
+  public void atacar()
+  {
+    Console.WriteLine($"{nombre} ataca con fuerza {fuerza}.")
+    
+  }
+  public override mostrarInfo()
+  {
+    base.mostrarInfo()
+    Console.WriteLine
+  }
+  }
+public class Mago: Personaje{
+  private float Inteligencia;
+
+  // Constructor
+  public Mago (string nombre, int nivel, int puntosDeVida, float inteligencia): base (nombre, nivel, puntosDeVida){
+    this.inteligencia= inteligencia;
   
 }
+
+  public class Sanador: Personaje{
+  private float sabiduria;
+
+  // Constructor
+  public Sanador (string nombre, int nivel, int puntosDeVida, float sabiduria): base (nombre, nivel, puntosDeVida){
+    this.sabiduria= sabiduria;
