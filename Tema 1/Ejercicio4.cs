b2cc6063f2d2d68dc53c59c835c135012af7090a
ireneb11 <irene.borja.u@gmail.com>
2025-09-29 16:07:05 +0200

    Create Ejercicio4.cs

---
+++ Tema 1/Ejercicio4.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Empleado{
+  // atributos
+  private int ID;
+  private float salario;
+  private int numeroHorasMax;
+
+  // Constructor
+  public Empleados(int ID, float salario, int numeroHorasMax){
+    this.ID = ID;
+    this.salario = salario;
+    this.numeroHorasMax = numeroHorasMax;
+  }
+
+  
+
+  
+}
+
+
+
+public class Program{
+  static void Main(string[]arg){
+    
+  }
+}
