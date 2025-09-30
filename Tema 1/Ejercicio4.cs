d08b814adb3f544f151e2f3a1ee2231fff03218e
ireneb11 <irene.borja.u@gmail.com>
2025-09-30 10:10:40 +0200

    Update Ejercicio4.cs

--- Tema 1/Ejercicio4.cs
+++ Tema 1/Ejercicio4.cs
@@ -5,13 +5,48 @@ class Empleado{
   public int ID {get; private set};
   private float salario;
   private int numeroHorasMax;
+  private bool estadoDadoAlta;
 
   // Constructor
-  public Empleados(int ID, float salario, int numeroHorasMax){
+  public Empleados(){
+    // inicializamos valores por defecto
+    ID = 0;
+    salario = 0f;
+    numeroHorasMax = 0;
+    estadoDadoAlta = false;
+  }
+  
+  // metodo para introducir datos
+  public void IntroducirDatos(int ID, float salario, int numeroHorasMax){
     this.ID = ID;
     this.salario = salario;
     this.numeroHorasMax = numeroHorasMax;
   }
+
+  public void VisualizarDatos(){
+    Console.WriteLine($"ID: {Id}");
+    Console.WriteLine($"Salario: {Salario}");
+    Console.WriteLine($"Max Horas Semana: {MaxHorasSemana}");
+    Console.WriteLine("Estado alta: ");
+    if (estadoDadoAlta){
+      Console.Write("Dado de Alta");
+    }else{
+      Console.Write("Dado de Baja");
+    }
+    
+  }
+
+  public void DarDeAlta(){
+    Console.WriteLine("Elija una de las siguientes opciones \n 1: Dar de alta \n 2: Dar de Baja");
+    Console.ReadLine
+    estadoDadoAlta = true
+    Console.WriteLine("Empleado dado de alta en la seguridad social y seguros.");
+  }
+
+  
+  
+
+  
 
   
 
