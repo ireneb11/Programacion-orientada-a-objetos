c00b68a952671f8021863aadfa8e1fb1952d0bd6
ireneb11 <irene.borja.u@gmail.com>
2025-10-15 13:01:23 +0200

    Update Ejercicio7.cs

--- Tema 2/Ejercicio7.cs
+++ Tema 2/Ejercicio7.cs
@@ -13,8 +13,7 @@ habido un error, y 1 si se ha creado correctamente y se han guardado los datos.
 
 
 using System;
-using System.IO;
-using System.Text;
+using System.IO;  
 
 class Program
 {
@@ -22,6 +21,7 @@ class Program
     {
         // guardamos los nombres en un array
         string[] nombres = { "María José", "Ana", "Luis", "Carlos" };
+
 
         string filePath = "nombres.txt";
 
@@ -34,23 +34,30 @@ class Program
         else {
             Console.WriteLine("Error al guardar el listado.");
         }
+
+        // muestra la ruta completa de dónde se ha guardado el archivo
+        Console.WriteLine("Archivo guardado en: "+ Path.GetFullPath(filePath));
     }
 
 
     public static int GuardarArray(string[] lista, string rutaDelArchivo)
     {
         // verifica si el archivo existe y si es asi lo elimina
-        if (File.Exists(rutaDelArchivo))
-        {
-            File.Delete(rutaDelArchivo);
-        }
-        // crea el archivo
-        using (FileStream fs = File.Create(rutaDelArchivo))
-        {
-
-            try
+        /* if (File.Exists(rutaDelArchivo))
             {
-                using (StreamWriter writer = new StreamWriter(rutaDelArchivo)) 
+                File.Delete(rutaDelArchivo);
+            }
+
+            using (FileStream fs = File.Create(rutaDelArchivo))
+
+        */ // no hace falta usarlo porque el streamwriter ya lo hace implicitamente
+        
+        
+
+
+        try
+        {
+                using (StreamWriter writer = new StreamWriter(rutaDelArchivo, false))  // usamos append= false -> para borrar lo que haya escrito en el archivo, si pusieramos true lo escribiriamos al final
                 {  // escribe en el archivo abriendo un straem y el using se encarga de cerrarlo
                     foreach (string elemento in lista)
                     {
@@ -65,8 +72,9 @@ class Program
                 Console.WriteLine("Error al guardar el listado: " + ex.Message);
                 return 0;
             }
-        }
+        
     }
     
  
 }
+
