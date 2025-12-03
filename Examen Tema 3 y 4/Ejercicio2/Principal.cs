1cef90201eec66076092ca8c328c9657ba99c0de
ireneb11 <irene.borja.u@gmail.com>
2025-12-03 07:59:50 +0100

    Create Principal class with inventory initialization

---
+++ Examen Tema 3 y 4/Ejercicio2/Principal.cs
@@ -0,0 +1,10 @@
+public class Principal{
+  List<Objeto> inventario= new List<Objeto>();
+  inventario.Add("Espada Maestra", "Armas",100);
+  inventario.Add( "Escudo Hyliano", "Defensas", 50);
+
+  
+  
+  
+  
+}
