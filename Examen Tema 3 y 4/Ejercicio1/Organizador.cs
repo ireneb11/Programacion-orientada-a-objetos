2fa00c53c97a3a48964c812bdfe215d2f602ed10
ireneb11 <irene.borja.u@gmail.com>
2025-12-03 10:40:10 +0100

    Enhance object categorization and uniqueness logic
    Refactor Organizador.cs to improve object organization and uniqueness handling.

--- Examen Tema 3 y 4/Ejercicio1/Organizador.cs
+++ Examen Tema 3 y 4/Ejercicio1/Organizador.cs
@@ -48,10 +48,73 @@ public class Organizador{
     // 3 lista doble con cada tipo
 
     Dictionary<string, List<Objeto>> diccionarioPorTipos = new Dictionary<string, List<Objeto>;
+    //2
+    foreach (Objeto obj in colaInicial){
+      if (obj == null){continue;}
+
+      string tipo = obj.tipo.ToLower();
+      if (tipo == null){
+        tipo ="";
+      }
+      if (!diccionarioPorTipos.ContainsKey(tipo)){
+        diccionarioPorTipos[tipo] = new List<Objeto
+      }
+      diccionarioPorTipos[tipo].Add(obj);
+    }
+
+    //3
+    LinkedList<Objeto> ObjetosPorTipo = new LinkedList<Objeto>();
+    foreach(KeyValuePair pareja in diccionarioPorTipos){
+      List<Objeto> list = pareja.Value;
+      foreach(Objeto obj in list){
+        ObjetosPorTipo.AddLast(obj)
+      }
+    }
+    return ObjetosPorTipo;
+    
+  }
+  // 4. Pila de Objetos Únicos 
+  public static Stack<Objeto>  ObjeterUnicos(Queue<Objeto> colaInicial){
+    if (colaInicial ==null|| colaInicial.Count==0) {
+      throw new ArgumentNullException("colaInicial","La cola no puede estar vacia");
+    }
+    //1 diccionario   - nombre, veces
+    //2 lista simpple de un tipo
+    Dictionary<string, int> diccionarioUnicos = new Dictionary<string, int>();
     foreach (Objeto obj in colaInicial){
       if (obj == null){continue;}
 
       
+      string nombre = obj.nombre;
+      if (nombre == null){nombre="";}
+      if (!diccionarioUnicos.ContainsKey(nombre)){
+        diccionarioUnicos[key] = 0;
+      }
+      diccionarioUnicos[key] += 1;
     }
+
+    //3 lista con los unicos
+    Stack<Objeto> ObjetosUnicos = new  Stack<Objeto>();
+
+    foreach(Object obj in colaInicial){
+      int veces;
+      if (obj.nombre == null){obj.nombre="";}
+      bool existe = diccionarioUnicos.TryGetKey(obj.nombre, out veces);
+      if (existe && veces==1){
+        ObjetosUnicos.Push(obj)
+      }
+
+      return ObjetosUnicos;
+    }
+
+    
+
+  
+    
+    
+    
   }
+  
+
+  
 }
