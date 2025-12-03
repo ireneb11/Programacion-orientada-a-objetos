221c310396a8ddaea12ab8b11d79a8916f8f7d16
ireneb11 <irene.borja.u@gmail.com>
2025-12-03 08:01:30 +0100

    Update Objeto.cs

--- Examen Tema 3 y 4/Ejercicio1/Objeto.cs
+++ Examen Tema 3 y 4/Ejercicio1/Objeto.cs
@@ -1,4 +1,13 @@
 public class Objeto{
+  // atributos
+  public string nombre {get; set;}
+  public string tipo {get; set;}
+  public int valor {get; set;}
+
+  public Objeto(string nombre,string tipo, int valor ){
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.valor = valor;
 
   
 }
