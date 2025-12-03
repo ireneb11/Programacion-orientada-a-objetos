1a67186a118aee434940c7e95a8ca0f3fd577cd6
ireneb11 <irene.borja.u@gmail.com>
2025-12-03 10:05:18 +0100

    Implement null check in ObjeterValiosos method
    Add null check for initial queue and throw exception if empty.

--- Examen Tema 3 y 4/Ejercicio1/Organizador.cs
+++ Examen Tema 3 y 4/Ejercicio1/Organizador.cs
@@ -1,5 +1,14 @@
 public class Organizador{
   public static Stack<Objeto>  ObjeterValiosos(Queue<Objeto> colaInicial, int umbral){
+    if (colaInicial ==null|| colaInicial.Count==0) {
+      throw new ArgumentNullException("colaInicial","La cola no puede estar vacia");
+    }
+    Stack
+    foreach(Objeto obj in colaInicial){
+      if(obj.valor >= umbral){
+        
+      }
+    }
     
   }
   
