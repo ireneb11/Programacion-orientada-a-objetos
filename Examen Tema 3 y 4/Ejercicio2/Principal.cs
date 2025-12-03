67ceb1cfe03967c6c657e038610734d97540af40
ireneb11 <irene.borja.u@gmail.com>
2025-12-03 09:19:40 +0100

    Refactor Principal class to include inventory methods
    Added methods to print and copy the inventory list.

--- Examen Tema 3 y 4/Ejercicio2/Principal.cs
+++ Examen Tema 3 y 4/Ejercicio2/Principal.cs
@@ -1,10 +1,28 @@
 public class Principal{
-  List<Objeto> inventario= new List<Objeto>();
-  inventario.Add("Espada Maestra", "Armas",100);
-  inventario.Add( "Escudo Hyliano", "Defensas", 50);
+  public static void Main(string[]arg){
+      List<Objeto> inventario= new List<Objeto>();
+      inventario.Add("Espada Maestra", "Armas",100);
+      inventario.Add( "Escudo Hyliano", "Defensas", 50);
 
+      Console.WriteLine("Inventario original:");
   
   
-  
+  }
+  public static void ImprimirLista(List<Objeto> lista){
+    if(lista== null ||lista.Count==0){return;}
+
+    foreach (Objeto obj in lista){
+      Console.WriteLine(obj.ToString());
+    }
+  }
+
+  public static List<Objeto> CopiaLista(List<Objeto> lista){
+    List<Objeto> copia = new List<Objeto>();
+    if(lista== null ||lista.Count==0){return;}
+
+    foreach (Objeto obj in lista){
+      copia.Add(obj);
+    }
+  }
   
 }
